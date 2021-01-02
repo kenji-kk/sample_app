2477491dbb939037977824ab8b57f2a515c11439
kenji <kenjix12x22@gmail.com>
2021-01-02 12:16:38 +0000

    bag solve

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,15 +1,12 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   
   def create
   end
 
   def destroy
   end
-  
-end
-
-  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -39,5 +36,7 @@ end
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
+  
 end
+
 
